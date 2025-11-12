35595d08adaf146703e989d07f22cc1aeb4d0f3f
Joe Du <Joe.Du@a2baustralia.com>
2025-11-12 16:42:25 +1100

    dummy change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Web Telop CICD testing 2025</p>
+        <h3>Web Telop CICD testing 2025</h3>
       </header>
     </div>
   );
